801544af5bf45186bb0c74a38648d20cf0cc2677
Deepan-GameDev <deepachandran495@gmail.com>
2026-06-30 13:22:34 +0530

    .

--- Assets/Scripts/TorchPickup.cs
+++ Assets/Scripts/TorchPickup.cs
@@ -3,10 +3,17 @@ using UnityEngine;
 public class TorchPickup : MonoBehaviour, IInteractable
 {
     public GameObject playerTorchLight;
+    public AudioClip pickupSound;
 
     public void Interact()
     {
+        // Turn on flashlight
         playerTorchLight.SetActive(true);
+
+        // Play pickup sound
+        AudioSource.PlayClipAtPoint(pickupSound, transform.position);
+
+        // Remove torch
         Destroy(gameObject);
     }
 }
