6dbef3b1be394bf50a5182fa0fa1742e1bc8f565
Deepan-GameDev <deepachandran495@gmail.com>
2026-07-03 15:34:56 +0530

    .

--- Assets/Scripts/TorchPickup.cs
+++ Assets/Scripts/TorchPickup.cs
@@ -4,17 +4,27 @@ public class TorchPickup : MonoBehaviour, IInteractable
 {
     public FlashlightController flashlightController;
     public GameObject flashlightButton;
+    public GameObject playerTorchModel;
     public AudioClip pickupSound;
 
     public void Interact()
     {
         if (pickupSound != null)
         {
-            AudioSource.PlayClipAtPoint(pickupSound, transform.position);
+            AudioSource.PlayClipAtPoint(
+                pickupSound,
+                transform.position
+            );
         }
 
         flashlightController.EnableFlashlight();
+
         flashlightButton.SetActive(true);
+
+        if (playerTorchModel != null)
+        {
+            playerTorchModel.SetActive(true);
+        }
 
         Destroy(gameObject);
     }
